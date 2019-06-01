a50c92ff45422626295d7bbcaa056148498e25c3
JensErhardt <jens-erhardt@web.de>
2019-06-01 13:55:45 +0200

    Implemented model for vehicle.

---
+++ CP-SAT-Product-Configurator/Models/VehicleModel.cs
@@ -0,0 +1,28 @@
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace CP_SAT_Product_Configurator.Models
+{
+  public class VehicleModel
+  {
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public string Id { get; set; }
+
+    [BsonElement("Name")]
+    public string ModelName { get; set; }
+
+    [BsonElement("Category")]
+    public VehicleCategory VehicleCategory { get; set; }
+
+    [BsonElement("Price")]
+    public decimal ModelPrice { get; set; }
+
+    [BsonElement("Features")]
+    public BsonArray ModelFeatures { get; set; }
+  }
+  public enum VehicleCategory
+  {
+    Compact, Sedan, Suv
+  };
+}
